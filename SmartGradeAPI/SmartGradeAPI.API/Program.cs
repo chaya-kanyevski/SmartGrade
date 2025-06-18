65ac26c2f26be118ff3b2b766cd35450ee53b211
chaya-kanyevski <c0533196732@gmail.com>
2025-06-18 15:56:18 +0300

    manage google

--- SmartGradeAPI/SmartGradeAPI.API/Program.cs
+++ SmartGradeAPI/SmartGradeAPI.API/Program.cs
@@ -20,45 +20,78 @@ var builder = WebApplication.CreateBuilder(args);
 Env.TraversePath().Load();
 if (FirebaseApp.DefaultInstance == null)
 {
-    var firebaseKeyPath = Environment.GetEnvironmentVariable("FIREBASE_KEY_PATH");
-
     GoogleCredential credential;
 
-    string projectId = Environment.GetEnvironmentVariable("FIREBASE_PROJECT_ID");
+    var firebaseJson = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS_JSON");
 
-    if (!string.IsNullOrEmpty(projectId))
+    if (!string.IsNullOrEmpty(firebaseJson))
     {
-        var firebaseCredentialObject = new
-        {
-            type = "service_account",
-            project_id = Environment.GetEnvironmentVariable("FIREBASE_PROJECT_ID"),
-            private_key_id = Environment.GetEnvironmentVariable("FIREBASE_PRIVATE_KEY_ID"),
-            private_key = Environment.GetEnvironmentVariable("FIREBASE_PRIVATE_KEY")?.Replace("\\n", "\n"),
-            client_email = Environment.GetEnvironmentVariable("FIREBASE_CLIENT_EMAIL"),
-            client_id = Environment.GetEnvironmentVariable("FIREBASE_CLIENT_ID"),
-            auth_uri = "https://accounts.google.com/o/oauth2/auth",
-            token_uri = "https://oauth2.googleapis.com/token",
-            auth_provider_x509_cert_url = "https://www.googleapis.com/oauth2/v1/certs",
-            client_x509_cert_url = Environment.GetEnvironmentVariable("FIREBASE_CLIENT_X509_CERT_URL")
-        };
-
-
-        var firebaseCredentialJson = System.Text.Json.JsonSerializer.Serialize(firebaseCredentialObject);
-        credential = GoogleCredential.FromJson(firebaseCredentialJson);
+        credential = GoogleCredential.FromJson(firebaseJson);
     }
     else
     {
         credential = GoogleCredential.FromFile("smartgrade-79f11-firebase-adminsdk-fbsvc-5b0d871db9.json");
     }
-    if (FirebaseApp.DefaultInstance == null)
-    {
-        FirebaseApp.Create(new AppOptions()
-        {
-            Credential = credential
-        });
-    }
 
+    FirebaseApp.Create(new AppOptions()
+    {
+        Credential = credential
+    });
 }
+
+//if (FirebaseApp.DefaultInstance == null)
+//{
+//    GoogleCredential credential;
+
+//    var firebaseJson = Environment.GetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS_JSON");
+
+//    if (!string.IsNullOrEmpty(firebaseJson))
+//    {
+//        credential = GoogleCredential.FromJson(firebaseJson);
+//    }
+//    else
+//    {
+//        credential = GoogleCredential.FromFile("smartgrade-79f11-firebase-adminsdk-fbsvc-5b0d871db9.json");
+//    }
+//    if (FirebaseApp.DefaultInstance == null)
+//    {
+//        FirebaseApp.Create(new AppOptions()
+//        {
+//            Credential = credential
+//        });
+//    }
+//var firebaseKeyPath = Environment.GetEnvironmentVariable("FIREBASE_KEY_PATH");
+
+//GoogleCredential credential;
+
+//string projectId = Environment.GetEnvironmentVariable("FIREBASE_PROJECT_ID");
+
+//if (!string.IsNullOrEmpty(projectId))
+//{
+//    var firebaseCredentialObject = new
+//    {
+//        type = "service_account",
+//        project_id = Environment.GetEnvironmentVariable("FIREBASE_PROJECT_ID"),
+//        private_key_id = Environment.GetEnvironmentVariable("FIREBASE_PRIVATE_KEY_ID"),
+//        private_key = Environment.GetEnvironmentVariable("FIREBASE_PRIVATE_KEY")?.Replace("\\n", "\n"),
+//        client_email = Environment.GetEnvironmentVariable("FIREBASE_CLIENT_EMAIL"),
+//        client_id = Environment.GetEnvironmentVariable("FIREBASE_CLIENT_ID"),
+//        auth_uri = "https://accounts.google.com/o/oauth2/auth",
+//        token_uri = "https://oauth2.googleapis.com/token",
+//        auth_provider_x509_cert_url = "https://www.googleapis.com/oauth2/v1/certs",
+//        client_x509_cert_url = Environment.GetEnvironmentVariable("FIREBASE_CLIENT_X509_CERT_URL")
+//    };
+
+
+//    var firebaseCredentialJson = System.Text.Json.JsonSerializer.Serialize(firebaseCredentialObject);
+//    credential = GoogleCredential.FromJson(firebaseCredentialJson);
+//}
+//else
+//{
+//    credential = GoogleCredential.FromFile("smartgrade-79f11-firebase-adminsdk-fbsvc-5b0d871db9.json");
+//}
+
+//}
 
 //if (FirebaseApp.DefaultInstance == null)
 //{
